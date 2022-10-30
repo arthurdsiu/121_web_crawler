332deb72fb39e85a7c25d65d0370145b3090e110
ChrisBryann <christopher_bryan@rocketmail.com>
2022-10-30 07:58:30 +0000

    .

--- scraper.py
+++ scraper.py
@@ -16,9 +16,9 @@ subDomainCount = dict()
 
 stopWords = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours	ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"}            
 
-domains = ["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu" ,"stat.uci.edu", "cbcl.ics.uci.edu"]
+domains = ["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu" ,"stat.uci.edu"]
 
-disallowQueriesDomains = {"swiki.ics.uci.edu", "wiki.ics.uci.edu", "archive.ics.uci.edu"}
+disallowQueriesDomains = ["swiki.ics.uci.edu", "wiki.ics.uci.edu", "archive.ics.uci.edu"]
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -49,7 +49,7 @@ def extract_next_links(url, resp):
     addTokens(soup)
 
     domain = urlparse(resp.url).hostname
-    if re.match(r'.*\.ics.uci.edu$', domain):
+    if re.match('.*\.ics\.uci\.edu$', domain):
         subDomainCount[domain] = subDomainCount.get(domain, 0) + 1
 
     longestPage(soup, url)
@@ -95,13 +95,9 @@ def is_valid(url):
         if isTrap(parsed):
             return False
         
-        #case sensitive matches
-        if re.search(r"\.java", parsed.geturl()):
-            return False
-        
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|png|tiff?|mid|mp2|mp3|mp4|mpg"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|ppsx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
@@ -122,12 +118,15 @@ def isTrap(parsed):
 
     path = parsed.path.lower()
     #check if there is pdf in between
-    if any(x in path for x in ['?replytocom=', '/pdf/', "#comment-"]):
+    if any(x in path for x in ['?replytocom=', '/pdf/', "#comment-", "events", "calendar"]):
         return True
 
-    #avoid any directory named pix
-    if (re.search('pix', path)):
-        return True
+    #check if it's in a calendar
+    # if 'wics' in path and bool(re.search('/events/.*?/', path)):
+    #     return True
+
+    # if 'today' in path and bool(re.search('/calendar/.*?/', path)):
+    #     return True
 
     # if re.match(r'\/calendar\/.+ | \/events\/.+', path) or re.match(r'.*?\/(.+?)\/.?\1.* | .*?\/(.+?)\/.?\2.*', path) or re.match(r'.*\..+\/', path):
     #     print('Path is trap:', path)
