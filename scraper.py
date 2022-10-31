2a14ccba54ed8127f39508faf47194ce340d7a91
Arthur D Siu <51488740+arthurdsiu@users.noreply.github.com>
2022-10-30 18:14:14 -0700

    Fixed get_logger to be thread safe

--- scraper.py
+++ scraper.py
@@ -27,7 +27,6 @@ shelveName = 'ans.shelve'
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    dumpAnswers()
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
@@ -57,12 +56,11 @@ def extract_next_links(url, resp):
     #update answers
     visitedPages.add(resp.url)
     addTokens(soup)
-
     domain = urlparse(resp.url).hostname
     if re.match(r'.*\.ics\.uci\.edu$', domain):
         subDomainCount[domain] = subDomainCount.get(domain, 0) + 1
-
     longestPage(soup, url)
+    dumpAnswers()
     #finish updating answers
 
     links =  soup.findAll("a")
@@ -117,7 +115,8 @@ def is_valid(url):
         + r"|shar|h|cpp|c|cp|makefile|py"
         + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$")
         
-        return not re.match(invalidPattern, parsed.path.lower()) and not any(re.match(invalidPattern, x) for x in parsed.query.lower().split('&'))
+        ret =  not re.match(invalidPattern, parsed.path.lower()) and not any(re.match(invalidPattern, x) for x in parsed.query.lower().split('&'))
+        return ret
         
     except TypeError:
         print ("TypeError for ", parsed)
@@ -172,6 +171,7 @@ def longestPage(soup, url):
 
 
 def dumpAnswers():
+    logger = get_logger("SCRAPER")
     try:
         file = open("answers.txt", "w")
         file.write(f"Q1: Number of unique URLs: {len(visitedPages)}\n")
@@ -194,6 +194,8 @@ def dumpAnswers():
         d['wordCount'] = wordCount
         d['subDomainCount'] = subDomainCount
         d['visitedPages'] = visitedPages
+        
+        logger.info(f"Saving answers shelve with visited pages of length {len(visitedPages)}")
         d.close()
 
     except Exception as e:
@@ -201,7 +203,7 @@ def dumpAnswers():
 
 def loadGlobals(name):
     logger = get_logger("SCRAPER")
-    logger.info("Logger called") 
+    logger.info("Logger called for opening answers shelve") 
     if os.path.exists(name):
         global longestPages, wordCount, subDomainCount, visitedPages
         try:
